71320957e36aa7c1297de97b9de454776fe1c2ee
Jason <jason422849464@gmail.com>
2016-05-17 15:59:28 +0800

    update demo

--- showcase/src/java/demo/jsf/DashboardMB.java
+++ showcase/src/java/demo/jsf/DashboardMB.java
@@ -1,8 +1,11 @@
 package demo.jsf;
 
 import com.primefaces.ext.base.web.bean.BaseDashboardMB;
+import java.util.HashMap;
+import java.util.Map;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import org.primefaces.context.RequestContext;
 
 /**
  *
@@ -12,4 +15,10 @@ import javax.faces.bean.ViewScoped;
 @ViewScoped
 public class DashboardMB extends BaseDashboardMB {
 
+    public void openSource() {
+        Map<String, Object> options = new HashMap<String, Object>();
+        options.put("maximizable", true);
+        options.put("width", "600");
+        RequestContext.getCurrentInstance().openDialog("dashboard_sources", options, null);
+    }
 }
