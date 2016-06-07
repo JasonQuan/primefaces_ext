190ca2382e165ebff8ff9b746f1e3023e2b8a5d5
chenquan <chenquan@Jason>
2016-06-08 00:08:28 +0800

    update config file path

--- primefaces-ext-showcase/src/main/resources/META-INF/init_dashboard.sql
+++ primefaces-ext-showcase/src/main/resources/META-INF/init_dashboard.sql
@@ -1,6 +1,7 @@
-INSERT INTO base_dashboard_model (`ID`,`CUSTOMEKEY`)  VALUES ('1','default');
-
-INSERT INTO base_panel_model (`ID`, `COLUMNINDEX`, `CUSTOMEKEY`, `HEADER`, `INCLUDE`, `ITEMINDEX`, `STYLE`, `VERSION`)
+delete from BASE_PANEL_MODEL;
+delete from BASE_DASHBOARD_MODEL;
+INSERT INTO BASE_DASHBOARD_MODEL (`ID`,`CUSTOMEKEY`)  VALUES ('1','default');
+INSERT INTO BASE_PANEL_MODEL (`ID`, `COLUMNINDEX`, `CUSTOMEKEY`, `HEADER`, `INCLUDE`, `ITEMINDEX`, `STYLE`, `VERSION`)
 VALUES (concat(char(round((rand())*25)+97),char(round((rand())*25)+65),char(round((rand())*25)+65),char(round((rand())*25)+65),char(round((rand())*25)+65)), 
 CEILING((RAND() * 6)) , 'default',
  concat(
